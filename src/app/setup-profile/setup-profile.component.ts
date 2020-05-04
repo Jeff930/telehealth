9e487ed479b4fcacb3e077fbc691aba0d2a88e3c
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-04 18:16:56 +0800

    Added setup profile component

---
+++ src/app/setup-profile/setup-profile.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-setup-profile',
+  templateUrl: './setup-profile.component.html',
+  styleUrls: ['./setup-profile.component.scss'],
+})
+export class SetupProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
