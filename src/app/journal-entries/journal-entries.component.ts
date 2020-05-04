ea620f8a1b77eba72e7784a254e55f883066899f
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-04 18:16:16 +0800

    Added Journal Entries component

---
+++ src/app/journal-entries/journal-entries.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-journal-entries',
+  templateUrl: './journal-entries.component.html',
+  styleUrls: ['./journal-entries.component.scss'],
+})
+export class JournalEntriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
