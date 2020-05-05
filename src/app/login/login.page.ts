b0d4b9d81fe520005213a8a3fd8db534770f5094
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-05 22:09:34 +0800

    Added functionality to login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {  NavController, LoadingController , AlertController, MenuController , Platform  } from '@ionic/angular';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { HttpClient, HttpErrorResponse , HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  login_form: FormGroup;
 
-  ngOnInit() {
+  constructor(
+    public navCtrl: NavController,
+    public loadingCtrl: LoadingController,
+    public formBuilder: FormBuilder,
+    public alertCtrl : AlertController,
+    public menuCtrl : MenuController,
+    public platform : Platform,
+    public http : HttpClient) {
+      console.log("cordova " + this.platform.is('cordova'));
+      console.log("android " + this.platform.is('android'));
+      this.menuCtrl.enable(false,'main-content');
+      if (this.platform.is('android') && this.platform.is('cordova')) {
+        console.log("Nav enabled");
+        this.menuCtrl.enable(true, 'loginMenu');
+      } else {
+        this.menuCtrl.enable(false, 'loginMenu');
+      }
+      this.menuCtrl.enable(false, 'myMenu');  
+    }
+
+    ngOnInit(){}
+    
+  ionViewWillLoad() {
+    this.login_form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      password: new FormControl('', Validators.required),
+    });
   }
 
 }
