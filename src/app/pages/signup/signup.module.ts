8794b40fe3a60ec64236d436d0e9fa9923666c8b
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-07 18:02:41 +0800

    Relocated Signup page

--- src/app/pages/signup/signup.module.ts
+++ src/app/pages/signup/signup.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -12,6 +13,8 @@ import { SignupPage } from './signup.page';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     IonicModule,
     SignupPageRoutingModule
   ],
---
+++ src/app/pages/signup/signup.page.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {  NavController, LoadingController , AlertController, MenuController , Platform  } from '@ionic/angular';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { HttpClient, HttpErrorResponse , HttpParams} from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.page.html',
+  styleUrls: ['./signup.page.scss'],
+})
+export class SignupPage implements OnInit {
+
+  signup_form: FormGroup;
+
+  constructor(
+    public navCtrl: NavController,
+    public loadingCtrl: LoadingController,
+    public formBuilder: FormBuilder,
+    public alertCtrl : AlertController,
+    public menuCtrl : MenuController,
+    public platform : Platform,
+    public http : HttpClient) {
+      console.log("cordova " + this.platform.is('cordova'));
+      console.log("android " + this.platform.is('android'));
+      this.menuCtrl.enable(false,'main-content');
+      // if (this.platform.is('android') && this.platform.is('cordova')) {
+      //   console.log("Nav enabled");
+      //   this.menuCtrl.enable(true, 'loginMenu');
+      // } else {
+      //   this.menuCtrl.enable(false, 'loginMenu');
+      // }
+      // this.menuCtrl.enable(false, 'myMenu');  
+
+      this.signup_form = this.formBuilder.group({
+        firstname: new FormControl('', Validators.compose([Validators.required])),
+        lastname: new FormControl('', Validators.compose([Validators.required])),
+        email: new FormControl('', Validators.compose([Validators.required])),
+        password: new FormControl('', Validators.required),
+        confirmpassword: new FormControl('', Validators.required),
+      });
+    }
+
+    ngOnInit(){}
+
+}
