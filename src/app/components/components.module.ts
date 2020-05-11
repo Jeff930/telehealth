b0cc594d41fffcd5df38c0e2389a18c6c17162df
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-11 20:36:06 +0800

    Added components to components module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {IonicModule} from '@ionic/angular';
 
-
+import { JournalEntriesComponent } from './journal-entries/journal-entries.component';
+import { SetupProfileComponent } from './setup-profile/setup-profile.component';
+import { ViewEntryComponent} from "./view-entry/view-entry.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SetupProfileComponent,
+    ViewEntryComponent,
+    JournalEntriesComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
+  ],
+  exports: [
+    SetupProfileComponent,
+    ViewEntryComponent,
+    JournalEntriesComponent
   ]
 })
 export class ComponentsModule { }
