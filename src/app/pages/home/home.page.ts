6ec2a6fc300611f68ab22e06a9d79b02f9d65cb3
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-11 20:37:30 +0800

    Home page clean up

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LoadingController , AlertController, MenuController , Platform  } from '@ionic/angular';
-import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
-import { HttpClient, HttpErrorResponse , HttpParams} from '@angular/common/http';
-import { Router } from '@angular/router';
-import { UserService } from '../../services/user.service';
+
 
 @Component({
   selector: 'app-home',
@@ -12,17 +8,8 @@ import { UserService } from '../../services/user.service';
 })
 export class HomePage implements OnInit {
 
-  constructor(private router: Router,
-    public loadingCtrl: LoadingController,
-    public formBuilder: FormBuilder,
-    public alertCtrl : AlertController,
-    public menuCtrl : MenuController,
-    public platform : Platform,
-    public http : HttpClient,
-    public userService : UserService) {
-      console.log("cordova " + this.platform.is('cordova'));
-      console.log("android " + this.platform.is('android'));
-      this.userService.showSidebar = true;
+  constructor() {
+      
     }
 
   ngOnInit() {
