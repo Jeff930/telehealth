2250a3abf28bbfb4f37bd9ea0dabc4f24a92a5c4
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-12 17:47:40 +0800

    Added Home page typescripts

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LoadingController , AlertController, MenuController , Platform  } from '@ionic/angular';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { HttpClient, HttpErrorResponse , HttpParams} from '@angular/common/http';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() {
-      
+  constructor(private router: Router,
+    public loadingCtrl: LoadingController,
+    public formBuilder: FormBuilder,
+    public alertCtrl : AlertController,
+    public menuCtrl : MenuController,
+    public platform : Platform,
+    public http : HttpClient,
+    public userService : UserService) {
+      console.log("cordova " + this.platform.is('cordova'));
+      console.log("android " + this.platform.is('android'));
+      this.userService.showSidebar = true;
     }
 
   ngOnInit() {
+  }
+
+  viewEntries(){
+    this.router.navigateByUrl('/view-entries');
   }
 
 }
