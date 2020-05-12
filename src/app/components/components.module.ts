4d7c8efcbed8f02e1e1563d9b8ef9338a87cf9b1
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-12 16:43:05 +0800

    Added new components to component module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,13 +4,17 @@ import {IonicModule} from '@ionic/angular';
 
 import { JournalEntriesComponent } from './journal-entries/journal-entries.component';
 import { SetupProfileComponent } from './setup-profile/setup-profile.component';
-import { ViewEntryComponent} from "./view-entry/view-entry.component";
+import { ViewEntryComponent} from './view-entry/view-entry.component';
+import { JournalCardComponent } from './journal-card/journal-card.component';
+import { QuoteCardComponent } from './quote-card/quote-card.component';
 
 @NgModule({
   declarations: [
     SetupProfileComponent,
     ViewEntryComponent,
-    JournalEntriesComponent
+    JournalEntriesComponent,
+    QuoteCardComponent,
+    JournalCardComponent
   ],
   imports: [
     CommonModule,
@@ -19,7 +23,9 @@ import { ViewEntryComponent} from "./view-entry/view-entry.component";
   exports: [
     SetupProfileComponent,
     ViewEntryComponent,
-    JournalEntriesComponent
+    JournalEntriesComponent,
+    QuoteCardComponent,
+    JournalCardComponent
   ]
 })
 export class ComponentsModule { }
