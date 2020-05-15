bd361f756e8d4b1457ae4dc90a806bc70afe880f
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-15 17:39:13 +0800

    Added basic validation to Create Entry

--- src/app/pages/create-entry/create-entry.page.ts
+++ src/app/pages/create-entry/create-entry.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController , AlertController, MenuController , Platform  } from '@ionic/angular';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { HttpClient, HttpErrorResponse , HttpParams} from '@angular/common/http';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-create-entry',
@@ -7,7 +12,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEntryPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+    public loadingCtrl: LoadingController,
+    public formBuilder: FormBuilder,
+    public alertCtrl : AlertController,
+    public menuCtrl : MenuController,
+    public platform : Platform,
+    public http : HttpClient,
+    public userService : UserService) {
+      console.log("cordova " + this.platform.is('cordova'));
+      console.log("android " + this.platform.is('android'));
+      this.userService.showSidebar = true;
+    }
 
   ngOnInit() {
   }
