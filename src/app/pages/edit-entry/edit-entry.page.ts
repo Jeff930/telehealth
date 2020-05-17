f8741f19aaa4605d4c35ce3dbcc6a8cd07f23b10
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-18 00:22:14 +0800

    Added navigation to Edit Entry

--- src/app/pages/edit-entry/edit-entry.page.ts
+++ src/app/pages/edit-entry/edit-entry.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-entry',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditEntryPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  saveChanges(){
+    this.router.navigateByUrl('/view-entry');
+  }
+
+  cancelEdit(){
+    this.router.navigateByUrl('/view-entry')
   }
 
 }
