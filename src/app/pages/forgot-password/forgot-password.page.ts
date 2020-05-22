5950800e337f7bf99ee0b9c19d6bad59332f76ce
noobpaul <paulgenesisbacuna@gmail.com>
2020-05-22 21:16:38 +0800

    added new password forms

--- src/app/pages/forgot-password/forgot-password.page.ts
+++ src/app/pages/forgot-password/forgot-password.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordPage implements OnInit {
 
-  constructor() { }
+  verification_number: number = 1234;
+  v_num_input: number;
+  num_verified: boolean = false;
+  wrong_num: boolean = false;
+  password_form: FormGroup;
+  constructor(
+    public router: Router,
+    public fb: FormBuilder
+  ) {
+    this.password_form = this.fb.group({
+      new_password: new FormControl('', Validators.required),
+      verify_password: new FormControl('', Validators.required)
+    });
+  }
 
   ngOnInit() {
+  }
+
+  submitRecovery(num_input) {
+    if (num_input == this.verification_number) {
+      this.num_verified = true;
+    } else {
+      this.wrong_num = true;
+    }
+  }
+
+  changePassword(data) {
+    // this.router.navigateByUrl('/login');
+    console.log(data);
   }
 
 }
