4c210804fea38dbc4714283e81e15e670796a7b9
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-27 17:40:35 +0800

    Created Signup API call

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -16,4 +16,16 @@ export class ApiService {
     console.log(JSON.stringify(form));
     return this.http.get<any>('http://localhost:5000/user-login/'+JSON.stringify(form));
   }
+
+  signupUser(formData) {
+    const body = new HttpParams()
+      .set('firstname', formData.firstname)
+      .set('lastname', formData.lastname)
+      .set('username', formData.username)
+      .set('email', formData.email)
+      .set('password', formData.password);
+    console.log(body);
+    return this.http.post<any>('http://localhost:5000/user-signup', body.toString(),
+     { headers: { 'Content-Type': 'application/x-www-form-urlencoded' }});
+  }
 }
