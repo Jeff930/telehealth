88a9d2d6907b9d768fe902a0e9cfed98f9b4c360
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-27 15:56:45 +0800

    Added Login API call to API service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse , HttpParams} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  loginUser(email, password) {
+    var form = {
+        email:email,
+        password:password,
+    }
+    console.log(JSON.stringify(form));
+    return this.http.get<any>('http://localhost:5000/user-login/'+JSON.stringify(form));
+  }
 }
