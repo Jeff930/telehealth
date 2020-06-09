bc5931a6905af2a40df98c1fcf1289e82cdc9305
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-06-09 17:14:53 +0800

    Image data is now accepted

--- src/app/components/journal-construct/journal-construct.component.ts
+++ src/app/components/journal-construct/journal-construct.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { ApiService } from 'src/app/services/api.service';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Crop } from '@ionic-native/crop/ngx';
@@ -14,8 +15,7 @@ import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-nati
 export class JournalConstructComponent implements OnInit {
 
   entry_form: FormGroup;
-  fileUrl: any = null;
-  respData: any;
+  imagePath;
 
   constructor(
     private router: Router,
@@ -23,43 +23,31 @@ export class JournalConstructComponent implements OnInit {
     private crop: Crop,
     private transfer: FileTransfer,
     public userService: UserService,
+    public apiService: ApiService,
     public formBuilder: FormBuilder,
     ) {
   }
 
   ngOnInit() {}
 
-  // cropUpload() {
-  //   this.imagePicker.getPictures({ maximumImagesCount: 1, outputType: 0 }).then((results) => {
-  //     for (let i = 0; i < results.length; i++) {
-  //         console.log('Image URI: ' + results[i]);
-  //         this.crop.crop(results[i], { quality: 100 })
-  //           .then(
-  //             newImage => {
-  //               console.log('new image path is: ' + newImage);
-  //               const fileTransfer: FileTransferObject = this.transfer.create();
-  //               const uploadOpts: FileUploadOptions = {
-  //                  fileKey: 'file',
-  //                  fileName: newImage.substr(newImage.lastIndexOf('/') + 1)
-  //               };
-  
-  //               // fileTransfer.upload(newImage, 'http://192.168.0.7:3000/api/upload', uploadOpts)
-  //               //  .then((data) => {
-  //               //    console.log(data);
-  //               //    this.respData = JSON.parse(data.response);
-  //               //    console.log(this.respData);
-  //               //    this.fileUrl = this.respData.fileUrl;
-  //               //  }, (err) => {
-  //               //    console.log(err);
-  //               //  });
-  //             },
-  //             error => console.error('Error cropping image', error)
-  //           );
-  //     }
-  //   }, (err) => { console.log(err); });
-  // }
-
   acceptImage(image){
     console.log(image);
+    const file: File = image.files[0];
+    const reader = new FileReader();
+    console.log(file);
+    reader.addEventListener('load', (event: any) => {
+      this.imagePath = event.target.result;
+      console.log(this.imagePath);
+    //   this.apiService.uploadImage(this.selectedFile.file).subscribe(
+    //     (res) => {
+        
+    //     },
+    //     (err) => {
+        
+    //     })
+     });
+
+    var test= reader.readAsDataURL(file);
+    console.log(test);
   }
 }
