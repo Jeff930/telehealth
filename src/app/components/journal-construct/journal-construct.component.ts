7f56c6dcf95a68616b1c9ea88ed843c240068dc1
Jeff930 <jefferson30.jdg@gmail.com>
2020-08-18 22:12:56 +0800

    Reworked on Fetch Image parsing

--- src/app/components/journal-construct/journal-construct.component.ts
+++ src/app/components/journal-construct/journal-construct.component.ts
@@ -24,12 +24,18 @@ export class JournalConstructComponent implements OnInit {
   ngOnInit() {
     if (this.userService.journalMode == "Create"){
       this.imagePaths = [];
+      this.images = [];
     }else{
       this.apiService.getTotalImage(this.userService.viewedEntry.EntryNo).subscribe(res => {
-        console.log(res['totalFiles']);
-        for (var i=0;i<res['totalFiles'];i++){
-          this.imagePaths.push('https://journal4life.com/api/v1/images/entries/'+this.userService.viewedEntry.EntryNo+'/'+this.userService.viewedEntry.EntryNo+"-"+i+".jpeg");
+        console.log(res);
+        this.imagePaths = res;
+        for (var i=0;i<this.imagePaths.length;i++){
+          this.images.push(btoa(this.imagePaths[i]).replace("+", "-").replace("/", "_"));
+          this.userService.entryImages = this.images;
         }
+        // for (var i=0;i<res['totalFiles'];i++){
+        //   this.imagePaths.push('https://journal4life.com/api/v1/images/entries/'+this.userService.viewedEntry.EntryNo+'/'+this.userService.viewedEntry.EntryNo+"-"+i+".jpeg");
+        // }
       });
     }
     console.log(this.imagePaths);
@@ -39,18 +45,24 @@ export class JournalConstructComponent implements OnInit {
 
   acceptImage(image){
     console.log(image);
+    console.log(this.userService.entryImages);
     for (var i=0;i<image.files.length;i++){ 
       var file:File = image.files[i];
       const reader = new FileReader();
       console.log(image.files.length);
       console.log("entered")
       reader.onload = (event: any) => {
+        console.log(this.userService.entryImages);
         console.log("entered")
         var tempImage = event.target.result;
+        console.log(tempImage);
         this.imagePaths.push(tempImage);
-        var image = btoa(tempImage).replace("+", "-").replace("/", "_")
+        var image = btoa(tempImage).replace("+", "-").replace("/", "_");
         this.images.push(image);
-        this.userService.entryImages = this.images;
+        console.log(this.userService.entryImages);
+        //this.userService.entryImages.push(image);
+        console.log(this.imagePaths);
+        console.log(this.userService.entryImages);
       };
       reader.readAsDataURL(file);      
     }
@@ -61,5 +73,7 @@ export class JournalConstructComponent implements OnInit {
     this.images.splice(index,1);
     this.userService.entryImages = this.images;
     console.log(this.imagePaths);
+    console.log(this.images);
+    console.log(this.userService.entryImages);
   }
 }
