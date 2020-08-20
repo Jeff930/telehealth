255f2b5f709d2fd3e7d2d11317dd9af63a5cfbf2
Jeff930 <jefferson30.jdg@gmail.com>
2020-08-20 16:08:19 +0800

    Added forgot password functionality

--- src/app/pages/forgot-password/forgot-password.page.ts
+++ src/app/pages/forgot-password/forgot-password.page.ts
@@ -9,11 +9,16 @@ import { FormBuilder, FormControl, Validators, FormGroup } from '@angular/forms'
 })
 export class ForgotPasswordPage implements OnInit {
 
-  verification_number: number = 1234;
+  verification_number: number = 657392;
   v_num_input: number;
+  e_num_input: "";
   num_verified: boolean = false;
   wrong_num: boolean = false;
   password_form: FormGroup;
+  step1 = true;
+  step2 = false;
+  step3 = false;
+  step4 = false;
   constructor(
     public router: Router,
     public fb: FormBuilder
@@ -29,15 +34,23 @@ export class ForgotPasswordPage implements OnInit {
 
   submitRecovery(num_input) {
     if (num_input == this.verification_number) {
-      this.num_verified = true;
+      this.step2 = false;
+    this.step3 = true;
     } else {
       this.wrong_num = true;
     }
   }
 
+  submitEmail(num_input) {
+    this.step1 = false;
+    this.step2 = true;
+  }
+
   changePassword(data) {
     // this.router.navigateByUrl('/login');
-    console.log(data);
+    // console.log(data);
+    this.step3 = false;
+    this.step4 = true;
   }
 
   goBackToLogin(){
