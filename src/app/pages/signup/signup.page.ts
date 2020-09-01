30cbdc4ed4d851e5d7b0515e680da2a9bf6f0967
Jeff930 <jefferson30.jdg@gmail.com>
2020-09-01 23:33:53 +0800

    Created Alerts for Signup page

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -43,9 +43,16 @@ export class SignupPage implements OnInit {
     ngOnInit(){}
 
     signup(formData){
+      this.loadingCtrl.create({
+        cssClass: 'yellow',
+        spinner:'circles'
+      }).then((res) => {
+        res.present();
       if ( formData.password === formData.confirmpassword) {
         if (formData.password.length < 6) {
           this.showShortPassword = true;
+          this.loadingCtrl.dismiss();
+          this.presentShort();
           this.showError = false;
           this.showSuccess = false;
           this.showPasswordMismatch = false;
@@ -53,11 +60,15 @@ export class SignupPage implements OnInit {
           this.apiService.signupUser(formData).subscribe( res => {
             console.log(res);
             if (res.affectedRows==1){
+              this.loadingCtrl.dismiss();
+              this.presentAlert();
               this.showSuccess = true;
               this.showShortPassword = false;
               this.showError = false;
               this.showPasswordMismatch = false;
             }else{
+              this.loadingCtrl.dismiss();
+              this.presentError();
               this.showSuccess = false;
               this.showShortPassword = false;
               this.showError = true;
@@ -66,11 +77,13 @@ export class SignupPage implements OnInit {
           });
         }
       } else {
+        this.loadingCtrl.dismiss();
+        this.presentInvalid();
           this.showError = false;
           this.showShortPassword = false;
           this.showSuccess = false;
           this.showPasswordMismatch = true; 
-      }
+      }});
     }
 
     login(){
@@ -81,6 +94,48 @@ export class SignupPage implements OnInit {
       this.userService.showMenubar = false;
       this.userService.showSidebar = false;
       console.log(this.userService.showMenubar,this.userService.showSidebar);
+    }
+
+    async presentAlert() {
+      const alert = await this.alertCtrl.create({
+        cssClass: 'alert',
+        header: 'Success!',
+        message: 'Your account has been created.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      const { role, data } = await alert.onDidDismiss();
+    }
+
+    async presentInvalid() {
+      const alert = await this.alertCtrl.create({
+        cssClass: 'alert',
+        header: 'Password Mismatch!',
+        message: 'Please check your password.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      const { role, data } = await alert.onDidDismiss();
+    }
+  
+    async presentError() {
+      const alert = await this.alertCtrl.create({
+        cssClass: 'alert',
+        header: 'Error!',
+        message: 'Failed to create account.',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
+
+    async presentShort() {
+      const alert = await this.alertCtrl.create({
+        cssClass: 'alert',
+        header: 'Invalid!',
+        message: 'Password is too short.',
+        buttons: ['OK']
+      });
+      await alert.present();
     }
 
 }
