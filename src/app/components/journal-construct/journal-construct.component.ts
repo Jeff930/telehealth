260d5dd187893b26056747fa9cee5289c8547492
Jeff930 <jefferson30.jdg@gmail.com>
2020-10-05 16:01:34 +0800

    Added dynamic image compression functionality

--- src/app/components/journal-construct/journal-construct.component.ts
+++ src/app/components/journal-construct/journal-construct.component.ts
@@ -3,7 +3,9 @@ import { UserService } from 'src/app/services/user.service';
 import { LoadingController } from '@ionic/angular';
 import { ApiService } from 'src/app/services/api.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import {NgxImageCompressService} from 'ngx-image-compress';
 import Quill from "quill";
+import { cpuUsage } from 'process';
 
 @Component({
   selector: 'app-journal-construct',
@@ -31,6 +33,7 @@ export class JournalConstructComponent implements OnInit {
     public apiService: ApiService,
     public formBuilder: FormBuilder,
     public loadingCtrl: LoadingController,
+    public imageCompress: NgxImageCompressService
     ) {
   }
 
@@ -39,6 +42,7 @@ export class JournalConstructComponent implements OnInit {
     if (this.userService.journalMode == "Create"){
       this.imagePaths = [];
       this.images = [];
+      this.userService.imagePaths = [];
     }else{
       this.apiService.getTotalImage(this.userService.viewedEntry.EntryNo).subscribe(res => {
         this.imagePaths = res;
@@ -55,6 +59,7 @@ export class JournalConstructComponent implements OnInit {
     if (this.userService.journalMode == "Create"){
       this.imagePaths = [];
       this.images = [];
+      this.userService.imagePaths = [];
     }else{
       this.loadingCtrl.create({
         cssClass: 'yellow',
@@ -66,7 +71,8 @@ export class JournalConstructComponent implements OnInit {
           this.userService.imagePaths = res;
           for (var i=0;i<this.imagePaths.length;i++){
             this.images.push(btoa(this.imagePaths[i]).replace("+", "-").replace("/", "_"));
-            this.userService.entryImages = this.images;
+            this.userService.entryImages.push(btoa(this.imagePaths[i]).replace("+", "-").replace("/", "_"));
+            console.log(this.images);
           }
         });
       })
@@ -74,19 +80,64 @@ export class JournalConstructComponent implements OnInit {
   }
 
   acceptImage(image){
-    for (var i=0;i<image.files.length;i++){ 
-      var file:File = image.files[i];
-      const reader = new FileReader();
-      reader.onload = (event: any) => {
-        var tempImage = event.target.result;
-        this.imagePaths.push(tempImage);
-        this.userService.imagePaths = this.imagePaths;
-        var image = btoa(tempImage).replace("+", "-").replace("/", "_");
-        this.images.push(image);
-        this.userService.entryImages = this.images;
-      };
-      reader.readAsDataURL(file);      
-    }
+    console.log("called");
+    this.loadingCtrl.create({
+      cssClass: 'yellow',
+      spinner:'circles',
+    }).then((res) => {
+      res.present();
+      console.log(this.userService.entryImages);
+      for (var i=0;i<image.files.length;i++){ 
+        var file:File = image.files[i];
+        const reader = new FileReader();
+        reader.onload = (event: any) => {
+          var tempImage = event.target.result;
+          console.warn('Size in bytes is now:', this.imageCompress.byteCount(tempImage));
+          if (this.imageCompress.byteCount(tempImage) < 250000){
+            console.log("called");
+            console.warn('Size in bytes is now:', this.imageCompress.byteCount(tempImage));
+            this.userService.imagePaths.push(tempImage);
+            this.imagePaths = this.userService.imagePaths;
+            var image = btoa(tempImage).replace("+", "-").replace("/", "_");
+            this.images.push(image);
+            this.userService.entryImages.push(image);
+            this.loadingCtrl.dismiss();
+            console.log(this.userService.entryImages);
+          }else{
+            if (this.imageCompress.byteCount(tempImage) > 900000){
+              console.log("called2");
+              this.imageCompress.compressFile(tempImage, "", 40, 40).then(
+              result => {
+                var compressedImage = result;
+                console.warn('Size in bytes is now:', this.imageCompress.byteCount(compressedImage));
+                this.userService.imagePaths.push(compressedImage);
+                this.imagePaths = this.userService.imagePaths;
+                var image = btoa(compressedImage).replace("+", "-").replace("/", "_");
+                this.images.push(image);
+                this.userService.entryImages.push(image);
+                this.loadingCtrl.dismiss();
+                console.log(this.userService.entryImages);
+              });
+            }else{
+              console.log("called3");
+              this.imageCompress.compressFile(tempImage, "", 60, 60).then(
+                result => {
+                  var compressedImage = result;
+                  console.warn('Size in bytes is now:', this.imageCompress.byteCount(compressedImage));
+                  this.userService.imagePaths.push(compressedImage);
+                  this.imagePaths = this.userService.imagePaths;
+                  var image = btoa(compressedImage).replace("+", "-").replace("/", "_");
+                  this.images.push(image);
+                  this.userService.entryImages.push(image);
+                  this.loadingCtrl.dismiss();
+                  console.log(this.userService.entryImages);
+                });
+            }
+          }
+        }
+        reader.readAsDataURL(file);     
+      } 
+    });
   }
 
   removeImage(index){
